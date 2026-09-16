ed5fba37989b08ba84f8a4c8d67b8930560d1e4b
Silver-Fox-App <larsen.c.greve@gmail.com>
2026-09-16 04:48:33 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "offenbarung-lernspiel-v1";
+
+const FILES_TO_CACHE = [
+  "/Offenbarung-Lernspiel/",
+  "/Offenbarung-Lernspiel/index.html",
+
+  "/Offenbarung-Lernspiel/Offb-Lernspiel-URL_DE/index.html",
+  "/Offenbarung-Lernspiel/Offb-Lernspiel-URL_DE/vorlage.png",
+
+  "/Offenbarung-Lernspiel/Offb-Lernspiel-URL_EN/index.html",
+  "/Offenbarung-Lernspiel/Offb-Lernspiel-URL_EN/vorlage.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
